923d3c86e7841ced24acb48c2b89d9ef7bfb970b
dandaso <dandaso@dandasonoMacBook-Pro.local>
2017-05-06 19:02:09 +0900

    add layers

--- mnist_vanilla/train.py
+++ mnist_vanilla/train.py
@@ -1,13 +1,11 @@
 import tensorflow as tf
 from two_layer_net import TwoLayerNet
 
-
 mnist = tf.contrib.learn.datasets.mnist.read_data_sets("MNIST_data/", one_hot=True)
-
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-epoch = 10
-batch_size = 30
+epoch = 100000
+batch_size = 200
 learning_rate = 0.1
 
 for i in range(epoch):
@@ -17,6 +15,6 @@ for i in range(epoch):
     for key in("W1", "b1", "W2", "b2"):
         network.params[key] -= learning_rate * grad[key]
 
-    loss = network.loss(batch[0], batch[1])
-    print(loss)
-
+    if i % 100 == 0:
+        acc = network.accuracy(mnist.test.images, mnist.test.labels)
+        print("accuracy: {0}".format(acc))
--- mnist_vanilla/two_layer_net.py
+++ mnist_vanilla/two_layer_net.py
@@ -1,40 +1,47 @@
+# coding: utf-8
 import numpy as np
 from gradient import Gradient
-from activation_function import ActivationFunction
+from activation_function import ActivationFunction as af
 from output import Output
+from layers import *
+
 from loss_function import LossFunction
+from collections import OrderedDict
+
 
 class TwoLayerNet:
 
-    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
         # 重みの初期化
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size) 
         self.params['b2'] = np.zeros(output_size)
 
-    def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
-    
-        a1 = np.dot(x, W1) + b1
-        z1 = ActivationFunction.sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = Output.softmax(a2)
+        # レイヤの生成
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+        self.lastLayer = SoftmaxWithLoss()
         
-        return y
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        
+        return x
         
     # x:入力データ, t:教師データ
     def loss(self, x, t):
         y = self.predict(x)
-        
-        return LossFunction.cross_entropy_error(y, t)
+        return self.lastLayer.forward(y, t)
     
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
         
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
@@ -44,35 +51,29 @@ class TwoLayerNet:
         loss_W = lambda W: self.loss(x, t)
         
         grads = {}
-        grads['W1'] = Gradient.numerical_gradient(loss_W, self.params['W1'])
-        grads['b1'] = Gradient.numerical_gradient(loss_W, self.params['b1'])
-        grads['W2'] = Gradient.numerical_gradient(loss_W, self.params['W2'])
-        grads['b2'] = Gradient.numerical_gradient(loss_W, self.params['b2'])
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
         
         return grads
         
     def gradient(self, x, t):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
-        grads = {}
-        
-        batch_num = x.shape[0]
-        
         # forward
-        a1 = np.dot(x, W1) + b1
-        z1 = ActivationFunction.sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = Output.softmax(a2)
-        
+        self.loss(x, t)
+
         # backward
-        dy = (y - t) / batch_num
-        grads['W2'] = np.dot(z1.T, dy)
-        grads['b2'] = np.sum(dy, axis=0)
+        dout = 1
+        dout = self.lastLayer.backward(dout)
         
-        da1 = np.dot(dy, W2.T)
-        dz1 = ActivationFunction.sigmoid_grad(a1) * da1
-        grads['W1'] = np.dot(x.T, dz1)
-        grads['b1'] = np.sum(dz1, axis=0)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 設定
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
 
         return grads
-
